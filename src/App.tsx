3095485ef3d1b72403323a2c466888fe9b878083
Mahfuz Ahmed <m4mahfuz@gmail.com>
2026-08-26 21:24:03 +0600

    Add nav background for light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       gridTemplateRows={{ base: 'auto 1fr auto', lg: 'auto 1fr' }}
       gridTemplateColumns={{ base: '1fr', lg: '250px 1fr' }}
     >
-      <GridItem gridArea="nav" p="4">
+      <GridItem gridArea="nav" bg={{ _light: 'gray.50', _dark: 'gray.800' }} p="4">
         <NavBar />
       </GridItem>
       <GridItem gridArea="aside" bg={{ _light: 'orange.300', _dark: 'orange.600' }} p="4" hideBelow="lg">
